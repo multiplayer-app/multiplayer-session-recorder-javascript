7629c9a242e3a7f08fda69267d1a3d9d4f07d231
Dmytro Shvaika <shvajka@gmail.com>
2025-07-17 15:11:53 +0300

    feat: masking updates

--- packages/opentelemetry/src/helpers/index.ts
+++ packages/opentelemetry/src/helpers/index.ts
@@ -1,5 +1,4 @@
 export { default as mask, sensitiveFields } from './mask'
 export { default as schemify } from './schemify'
 export * from './is-gzip'
-export * from './mask-headers'
 export * from './id-generator'
