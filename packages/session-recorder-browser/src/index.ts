33fe6dc85ee188dd58788ac34786c408747e5c95
Gegham Khachatryan <gegham.k90@gmail.com>
2025-07-28 17:34:45 +0400

    fix: include types in browser lib

--- packages/session-recorder-browser/src/index.ts
+++ packages/session-recorder-browser/src/index.ts
@@ -2,7 +2,16 @@ import './patch'
 import { setupListeners } from './listeners'
 import { recorderEventBus } from './eventBus'
 import { SessionRecorder } from './sessionRecorder'
-export * from '@multiplayer-app/session-recorder-common'
+export {
+  ATTR_MULTIPLAYER_SESSION_ID,
+  ATTR_MULTIPLAYER_HTTP_REQUEST_BODY,
+  ATTR_MULTIPLAYER_HTTP_RESPONSE_BODY,
+  ATTR_MULTIPLAYER_HTTP_REQUEST_HEADERS,
+  ATTR_MULTIPLAYER_HTTP_RESPONSE_HEADERS,
+  SessionType,
+  SessionRecorderIdGenerator,
+  SessionRecorderHttpTraceExporterBrowser,
+} from '@multiplayer-app/session-recorder-common'
 
 const SessionRecorderInstance = new SessionRecorder()
 
