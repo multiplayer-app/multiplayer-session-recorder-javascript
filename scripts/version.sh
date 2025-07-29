c9b47a32d343772cf065d5694462a055a1d6dd86
Gegham Khachatryan <gegham.k90@gmail.com>
2025-07-29 10:19:30 +0400

    fix: update version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -8,16 +8,21 @@ echo "🔢 Incrementing patch versions..."
 echo "  🔧 Versioning session-recorder-common..."
 cd packages/session-recorder-common
 npm version patch --no-git-tag-version --ignore-scripts
+COMMON_VERSION=$(node -p "require('./package.json').version")
 cd ../..
 
 echo "  🌐 Versioning session-recorder-browser..."
 cd packages/session-recorder-browser
 npm version patch --no-git-tag-version --ignore-scripts
+# Update devDependencies
+sed -i '' "s/\"@multiplayer-app\/session-recorder-common\": \"[^\"]*\"/\"@multiplayer-app\/session-recorder-common\": \"$COMMON_VERSION\"/" package.json
 cd ../..
 
 echo "  🖥️  Versioning session-recorder-node..."
 cd packages/session-recorder-node
 npm version patch --no-git-tag-version --ignore-scripts
+# Update dependencies
+sed -i '' "s/\"@multiplayer-app\/session-recorder-common\": \"[^\"]*\"/\"@multiplayer-app\/session-recorder-common\": \"$COMMON_VERSION\"/" package.json
 cd ../..
 
-echo "✅ All packages versioned successfully!"
+echo "✅ All packages versioned and dependencies updated successfully!"
