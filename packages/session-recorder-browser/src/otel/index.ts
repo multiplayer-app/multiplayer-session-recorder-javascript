c6cdfe9a3ec3916d508f437dc273d5d846945d8f
Gegham Khachatryan <gegham.k90@gmail.com>
2025-08-23 10:42:10 +0400

    feat: update spanExporter configuration and fix build fail

--- packages/session-recorder-browser/src/otel/index.ts
+++ packages/session-recorder-browser/src/otel/index.ts
@@ -20,28 +20,93 @@ export class TracerBrowserSDK {
   private tracerProvider?: WebTracerProvider
   private config?: TracerBrowserConfig
   private allowedElements = new Set<string>(['A', 'BUTTON'])
+  private idGenerator?: SessionRecorderIdGenerator
   private sessionId = ''
-  private idGenerator
+  private isRegistered = false
+  private spanExporterProcessors: SpanProcessor[] = []
 
   constructor() { }
+
+
+  init(options: TracerBrowserConfig): void {
+    this.config = options
+
+    this.idGenerator = new SessionRecorderIdGenerator({
+      autoDocTracesRatio: options.docTraceRatio || OTEL_MP_DOC_TRACE_RATIO,
+    })
+
+    this.tracerProvider = this.createTracerProvider()
+
+    if (this.config.apiKey) {
+      this.addBatchSpanExporter(this.config.apiKey)
+    }
+
+    this.registerTracerProvider()
+    this.registerInstrumentations()
+  }
+
+  start(
+    sessionId,
+    sessionType: SessionType,
+  ): void {
+    if (!this.tracerProvider) {
+      throw new Error(
+        'Configuration not initialized. Call init() before start().',
+      )
+    }
+
+    this.setSessionId(sessionId, sessionType)
+  }
+
+  stop(): void {
+    if (!this.tracerProvider) {
+      throw new Error(
+        'Configuration not initialized. Call init() before start().',
+      )
+    }
+
+    this.setSessionId('')
+  }
+
+
+  addBatchSpanExporter(apiKey: string): void {
+    if (!this.tracerProvider) {
+      throw new Error(
+        'Configuration not initialized. Call init() before start().',
+      )
+    }
+
+    const newSpanProcessor = new BatchSpanProcessor(
+      new SessionRecorderHttpTraceExporterBrowser({
+        apiKey,
+        url: `${this.config?.exporterApiBaseUrl}/v1/traces`,
+        usePostMessageFallback: this.config?.usePostMessageFallback,
+      }),
+    )
+
+    this.spanExporterProcessors = [newSpanProcessor]
+    this.tracerProvider = this.createTracerProvider()
+
+    this.registerTracerProvider()
+    this.registerInstrumentations()
+  }
 
   private setSessionId(
     sessionId: string,
     sessionType: SessionType = SessionType.PLAIN,
   ) {
     this.sessionId = sessionId
-    this.idGenerator.setSessionId(sessionId, sessionType)
+    this.idGenerator?.setSessionId(sessionId, sessionType)
   }
 
-  init(options: TracerBrowserConfig): void {
-    this.config = options
+  private createTracerProvider(): WebTracerProvider {
+    if (!this.config) {
+      throw new Error('Configuration not initialized. Call init() before creating tracer provider.')
+    }
+
     const { application, version, environment } = this.config
 
-    this.idGenerator = new SessionRecorderIdGenerator({
-      autoDocTracesRatio: options.docTraceRatio || OTEL_MP_DOC_TRACE_RATIO,
-    })
-
-    this.tracerProvider = new WebTracerProvider({
+    return new WebTracerProvider({
       resource: new Resource({
         [SemanticAttributes.SEMRESATTRS_SERVICE_NAME]: application,
         [SemanticAttributes.SEMRESATTRS_SERVICE_VERSION]: version,
@@ -50,18 +115,36 @@ export class TracerBrowserSDK {
       idGenerator: this.idGenerator,
       sampler: new SessionRecorderTraceIdRatioBasedSampler(this.config.sampleTraceRatio),
       spanProcessors: [
-        this._getSpanSessionIdProcessor(),
+        this.getSpanSessionIdProcessor(),
+        ...this.spanExporterProcessors,
       ],
     })
+  }
 
-    if (this.config.apiKey) {
-      this.addBatchSpanExporter(this.config.apiKey)
-    }
+  private registerTracerProvider(): void {
+    if (this.isRegistered || !this.tracerProvider) return
 
     this.tracerProvider.register({
-      // contextManager: new ZoneContextManager(),
       propagator: new W3CTraceContextPropagator(),
     })
+    this.isRegistered = true
+  }
+
+  private getSpanSessionIdProcessor(): SpanProcessor {
+    return {
+      forceFlush: () => Promise.resolve(),
+      onEnd: () => { },
+      shutdown: () => Promise.resolve(),
+      onStart: (span) => {
+        if (this.sessionId?.length) {
+          span.setAttribute(ATTR_MULTIPLAYER_SESSION_ID, this.sessionId)
+        }
+      },
+    }
+  }
+
+  private registerInstrumentations(): void {
+    if (!this.tracerProvider) return
 
     registerInstrumentations({
       tracerProvider: this.tracerProvider,
@@ -71,9 +154,9 @@ export class TracerBrowserSDK {
             clearTimingResources: true,
             ignoreUrls: [
               ...OTEL_IGNORE_URLS,
-              ...(this.config.ignoreUrls || []),
+              ...(this.config?.ignoreUrls || []),
             ],
-            propagateTraceHeaderCorsUrls: options.propagateTraceHeaderCorsUrls,
+            propagateTraceHeaderCorsUrls: this.config?.propagateTraceHeaderCorsUrls,
             applyCustomAttributesOnSpan: (span, xhr) => {
               if (!this.config) return
 
@@ -110,9 +193,9 @@ export class TracerBrowserSDK {
             clearTimingResources: true,
             ignoreUrls: [
               ...OTEL_IGNORE_URLS,
-              ...(this.config.ignoreUrls || []),
+              ...(this.config?.ignoreUrls || []),
             ],
-            propagateTraceHeaderCorsUrls: options.propagateTraceHeaderCorsUrls,
+            propagateTraceHeaderCorsUrls: this.config?.propagateTraceHeaderCorsUrls,
             applyCustomAttributesOnSpan: async (span, request, response) => {
               if (!this.config) return
 
@@ -163,58 +246,5 @@ export class TracerBrowserSDK {
         }),
       ],
     })
-  }
-
-  start(
-    sessionId,
-    sessionType: SessionType,
-  ): void {
-    if (!this.tracerProvider) {
-      throw new Error(
-        'Configuration not initialized. Call init() before start().',
-      )
-    }
-
-    this.setSessionId(sessionId, sessionType)
-  }
-
-  stop(): void {
-    if (!this.tracerProvider) {
-      throw new Error(
-        'Configuration not initialized. Call init() before start().',
-      )
-    }
-
-    this.setSessionId('')
-  }
-
-
-  addBatchSpanExporter(apiKey: string): void {
-    if (!this.tracerProvider) {
-      throw new Error(
-        'Configuration not initialized. Call init() before start().',
-      )
-    }
-
-    this.tracerProvider.addSpanProcessor(new BatchSpanProcessor(
-      new SessionRecorderHttpTraceExporterBrowser({
-        apiKey,
-        url: `${this.config?.exporterApiBaseUrl}/v1/traces`,
-        usePostMessageFallback: this.config?.usePostMessageFallback,
-      }),
-    ))
-  }
-
-  private _getSpanSessionIdProcessor(): SpanProcessor {
-    return {
-      forceFlush: () => Promise.resolve(),
-      onEnd: () => { },
-      shutdown: () => Promise.resolve(),
-      onStart: (span) => {
-        if (this.sessionId?.length) {
-          span.setAttribute(ATTR_MULTIPLAYER_SESSION_ID, this.sessionId)
-        }
-      },
-    }
   }
 }
