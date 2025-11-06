79a93d4cedb1959a24f1b5f89209b0d15daaa379
Gegham Khachatryan <gegham.k90@gmail.com>
2025-11-06 11:13:35 +0400

    feat: browser package bundler

--- packages/session-recorder-react/src/index.ts
+++ packages/session-recorder-react/src/index.ts
@@ -1,10 +1,8 @@
-import SessionRecorderBrowser from '@multiplayer-app/session-recorder-browser';
-
 export * from '@multiplayer-app/session-recorder-browser';
+export { default } from '@multiplayer-app/session-recorder-browser';
+
 export * from './context/SessionRecorderContext';
 export * from './context/useSessionRecorderStore';
 
 export { useNavigationRecorder } from './navigation'
 export type { UseNavigationRecorderOptions } from './navigation'
-
-export default SessionRecorderBrowser
