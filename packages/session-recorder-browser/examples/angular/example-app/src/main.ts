27c6531d150645dfc27fd559eec6ff3d254470d8
Gegham Khachatryan <gegham.k90@gmail.com>
2026-02-13 10:53:34 +0400

    feat: remove placehodlder api key

--- packages/session-recorder-browser/examples/angular/example-app/src/main.ts
+++ packages/session-recorder-browser/examples/angular/example-app/src/main.ts
@@ -6,12 +6,9 @@ import SessionRecorder from '@multiplayer-app/session-recorder-browser';
 // Initialize Multiplayer Session Recorder before bootstrapping Angular (Quick Start)
 SessionRecorder.init({
   version: '0.0.1',
-  apiKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnRlZ3JhdGlvbiI6IjY5MTFhMGEyNjRkMjk4OWVjNzNlMDVlNCIsIndvcmtzcGFjZSI6IjY4NGMzYmYwYjQ2MGUzMmY3YWJmZjRlMSIsInByb2plY3QiOiI2ODRjM2M0MmI0NjBlMzJmN2FiZmY1YzgiLCJ0eXBlIjoiT1RFTCIsImlhdCI6MTc2Mjc2MjkxNH0.j1JHKC_EgUzmssyhjNUi_jt9Qk6yCuwjiIS64io9GCQ',
+  apiKey: 'YOUR_API_KEY',
   environment: 'production',
   application: 'angular-example',
-  apiBaseUrl: 'http://localhost',
-  exporterEndpoint: 'http://localhost',
   propagateTraceHeaderCorsUrls: [
     // new RegExp('https://your.backend.api.domain', 'i'),
     new RegExp('https://jsonplaceholder.typicode.com', 'i'),
