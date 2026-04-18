e8c06242cf65f34d6606e43dd1b700ae4e78bd34
Gegham Khachatryan <gegham.k90@gmail.com>
2026-04-18 18:52:39 +0400

    fix lint

--- packages/cli/src/logger.ts
+++ packages/cli/src/logger.ts
@@ -1,8 +1,9 @@
+/* eslint-disable no-console -- single allowed sink for CLI stdout/stderr */
 const logger = {
   info: (msg: string, ...args: unknown[]) => console.log(msg, ...args),
   error: (msg: string, ...args: unknown[]) => console.error(msg, ...args),
   warn: (msg: string, ...args: unknown[]) => console.warn(msg, ...args),
-  debug: (msg: string, ...args: unknown[]) => console.debug(msg, ...args),
+  debug: (msg: string, ...args: unknown[]) => console.debug(msg, ...args)
 }
 
 export default logger
