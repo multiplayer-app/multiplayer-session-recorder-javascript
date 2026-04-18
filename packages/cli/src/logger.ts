ef548449b0989fdd5ef1c8ea777d316122c51680
Gegham Khachatryan <gegham.k90@gmail.com>
2026-04-18 18:53:31 +0400

    fix: lint

--- packages/cli/src/logger.ts
+++ packages/cli/src/logger.ts
@@ -3,7 +3,7 @@ const logger = {
   info: (msg: string, ...args: unknown[]) => console.log(msg, ...args),
   error: (msg: string, ...args: unknown[]) => console.error(msg, ...args),
   warn: (msg: string, ...args: unknown[]) => console.warn(msg, ...args),
-  debug: (msg: string, ...args: unknown[]) => console.debug(msg, ...args)
+  debug: (msg: string, ...args: unknown[]) => console.debug(msg, ...args),
 }
 
 export default logger
